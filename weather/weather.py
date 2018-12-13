08bde5fd056683ac86c82c96e34f3b9658db2bb6
Haim Elbaz <haim1321@gmail.com>
2018-12-13 20:38:20 +0200

    updated dynamic addition of fields into json argument

--- weather/weather.py
+++ weather/weather.py
@@ -5,8 +5,25 @@ from pprint import pprint
 with open('city.list.json') as f:                    #open json file and read it
     data = json.load(f)
 
+obj_json = {
+    'city': '',
+    'Temperature': '',
+    'Humidity': ''
+}
+
+general_json = {
+    'January':[
+    ],
+    'April':[
+    ],
+    'June':[
+    ],
+    'October':[
+    ]    
+}
 cities = ['Washington', 'Chile', 'Bordeaux', 'Catalunya', 'Madrid']
 dates = { 'January':'1515870589', 'April':'1523646589', 'June':'1528916989', 'October':'1542136189'}
+
 for city in cities:                             #for each city to search
     for dict in data:                           #search in the json data for it, and return the cityID
         if dict['name'] == city:
@@ -15,14 +32,15 @@ for city in cities:                             #for each city to search
             cityLong = str(dict['coord']['lon'])
 
     #https://api.darksky.net/forecast/[key]/[latitude],[longitude],[time] 
-    #for dateKey, dateVal in dates.iteritems():
     for date in dates:
         r = requests.get('https://api.darksky.net/forecast/a7896cfc9d0a3c6e26fecf1b8b560794/'+cityLat+','+cityLong+','+dates[date])            
         json_object = r.json()                                               #save it as a json object
         temp_c = ((json_object['currently']['temperature'] - 32) * (5/9))        #take the degress (supplied in fahrenheit) and convert them to celcious
         temp_h = json_object['daily']['data'][0]['humidity']
-        print(city)
-        print("Month: "+date)
-        print("Temperature: "+str(temp_c))
-        print("Humidity: "+str(temp_h))
-        print('********************')
+        
+        obj_json = {'city':city, 'Temperature':temp_c, 'Humidity':temp_h}
+        general_json[date].append(obj_json) 
+
+        
+json_data = json.dumps(general_json)
+pprint(json_data)
