489d0c3aba96f5820a7226751b4ed63f5ebba62c
Haim Elbaz <haim1321@gmail.com>
2018-12-13 19:46:52 +0200

    Update weather.py

--- weather/weather.py
+++ weather/weather.py
@@ -1,7 +1,28 @@
-import requests #to process the request in a get method via the API
+import requests                                 #to process the request in a get method via the API
+import json
+from pprint import pprint
 
-cityID = '7302001'  #an example code for a specific city - there is a full city list in the API 
-r = requests.get('http://api.openweathermap.org/data/2.5/weather?id='+cityID+'&appid=eee8747eb58c05ad3f545d00d778fd0a') #get the response using the city code
-json_object = r.json()  #save it as a json object
-temp_c = (float(json_object['main']['temp']) -273.15) #take the degress (supplied in kalvin) and convert them to celcious
-print(temp_c) #print them to the screen (just to check)
+with open('city.list.json') as f:                    #open json file and read it
+    data = json.load(f)
+
+cities = ['Washington', 'Chile', 'Bordeaux', 'Catalunya', 'Madrid']
+dates = { 'January':'1515870589', 'April':'1523646589', 'June':'1528916989', 'October':'1542136189'}
+for city in cities:                             #for each city to search
+    for dict in data:                           #search in the json data for it, and return the cityID
+        if dict['name'] == city:
+            cityID = str(dict['id'])
+            cityLat = str(dict['coord']['lat'])
+            cityLong = str(dict['coord']['lon'])
+
+    #https://api.darksky.net/forecast/[key]/[latitude],[longitude],[time] 
+    #for dateKey, dateVal in dates.iteritems():
+    for date in dates:
+        r = requests.get('https://api.darksky.net/forecast/a7896cfc9d0a3c6e26fecf1b8b560794/'+cityLat+','+cityLong+','+dates[date])            
+        json_object = r.json()                                               #save it as a json object
+        temp_c = ((json_object['currently']['temperature'] - 32) * (5/9))        #take the degress (supplied in fahrenheit) and convert them to celcious
+        temp_h = json_object['daily']['data'][0]['humidity']
+        print(city)
+        print("Month: "+date)
+        print("Temperature: "+str(temp_c))
+        print("Humidity: "+str(temp_h))
+        print('********************')
